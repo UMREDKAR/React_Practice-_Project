1be30187a2723c10689eafb1b554740d2ef71639
UMREDKAR <106841969+UMREDKAR@users.noreply.github.com>
2023-07-17 04:27:50 +0530

    props input

---
+++ Input.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Input(props)
+{
+   return(
+    <div>
+        <label>{props.label}</label>
+        <input type={props.type} id={props.id}
+        value={props.value} 
+        onChange={props.onChange}/>
+    </div>
+   );
+}
+
+export default Input;
